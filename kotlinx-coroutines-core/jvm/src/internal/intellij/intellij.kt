324f41a2430ceb34c82b6aa375acc9236c65998f
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2024-03-31 15:00:24 +0200

    IJPL-149823 Parallelism compensation for CoroutineDispatchers and runBlocking

--- kotlinx-coroutines-core/jvm/src/internal/intellij/intellij.kt
+++ kotlinx-coroutines-core/jvm/src/internal/intellij/intellij.kt
@@ -1,6 +1,10 @@
 package kotlinx.coroutines.internal.intellij
 
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.internal.softLimitedParallelism as softLimitedParallelismImpl
+import kotlinx.coroutines.internal.SoftLimitedDispatcher
+import kotlinx.coroutines.Dispatchers
 
 /**
  * [IntellijCoroutines] exposes the API added as part of IntelliJ patches.
@@ -8,5 +12,14 @@ import kotlinx.coroutines.InternalCoroutinesApi
  */
 @InternalCoroutinesApi
 public object IntellijCoroutines {
-
+    /**
+     * Constructs a [SoftLimitedDispatcher] from the specified [CoroutineDispatcher].
+     * [SoftLimitedDispatcher] behaves as [LimitedDispatcher][kotlinx.coroutines.internal.LimitedDispatcher] but allows
+     * temporarily exceeding the parallelism limit in case [parallelism compensation][kotlinx.coroutines.scheduling.withCompensatedParallelism]
+     * was requested (e.g., by [kotlinx.coroutines.runBlocking]).
+     *
+     * This extension can only be used on instances of [Dispatchers.Default], [Dispatchers.IO] and also on what this extension
+     * has returned. Throws [UnsupportedOperationException] if [this] does not support parallelism compensation mechanism.
+     */
+    public fun CoroutineDispatcher.softLimitedParallelism(parallelism: Int): CoroutineDispatcher = softLimitedParallelismImpl(parallelism)
 }
