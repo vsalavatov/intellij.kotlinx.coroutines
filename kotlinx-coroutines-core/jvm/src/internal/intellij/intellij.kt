ed2d57f498393e0047f93ac62eb50fdc4f815e0c
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2024-03-31 15:00:24 +0200

    IJPL-149823 Parallelism compensation for CoroutineDispatchers and runBlocking

--- kotlinx-coroutines-core/jvm/src/internal/intellij/intellij.kt
+++ kotlinx-coroutines-core/jvm/src/internal/intellij/intellij.kt
@@ -1,10 +1,13 @@
 package kotlinx.coroutines.internal.intellij
 
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.internal.softLimitedParallelism as softLimitedParallelismImpl
+import kotlinx.coroutines.internal.SoftLimitedDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlin.coroutines.*
 
 internal val currentContextThreadLocal: ThreadLocal<CoroutineContext?> = ThreadLocal.withInitial { null }
-
 
 /**
  * [IntellijCoroutines] exposes the API added as part of IntelliJ patches.
@@ -22,4 +25,15 @@ public object IntellijCoroutines {
     public fun currentThreadCoroutineContext(): CoroutineContext? {
         return currentContextThreadLocal.get()
     }
+
+    /**
+     * Constructs a [SoftLimitedDispatcher] from the specified [CoroutineDispatcher].
+     * [SoftLimitedDispatcher] behaves as [LimitedDispatcher][kotlinx.coroutines.internal.LimitedDispatcher] but allows
+     * temporarily exceeding the parallelism limit in case [parallelism compensation][kotlinx.coroutines.scheduling.withCompensatedParallelism]
+     * was requested (e.g., by [kotlinx.coroutines.runBlocking]).
+     *
+     * This extension can only be used on instances of [Dispatchers.Default], [Dispatchers.IO] and also on what this extension
+     * has returned. Throws [UnsupportedOperationException] if [this] does not support parallelism compensation mechanism.
+     */
+    public fun CoroutineDispatcher.softLimitedParallelism(parallelism: Int): CoroutineDispatcher = softLimitedParallelismImpl(parallelism)
 }
---
+++ kotlinx-coroutines-core/jvm/src/scheduling/parallelismCompensation.kt
@@ -0,0 +1,20 @@
+package kotlinx.coroutines.scheduling
+
+/**
+ * Introduced as part of IntelliJ patches.
+ *
+ * Increases the parallelism limit of the coroutine dispatcher associated with the current thread for the duration of [body] execution.
+ * After the [body] completes, the effective parallelism may stay higher than the associated limit, but it is said
+ * that eventually it will adjust to meet it.
+ */
+internal fun <T> withCompensatedParallelism(body: () -> T): T {
+    // CoroutineScheduler.Worker implements ParallelismCompensation
+    val worker = Thread.currentThread() as? ParallelismCompensation
+        ?: return body()
+    worker.increaseParallelismAndLimit()
+    try {
+        return body()
+    } finally {
+        worker.decreaseParallelismLimit()
+    }
+}
