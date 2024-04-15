730ba1e4ab810ba6244b0127b586ccd88a66a013
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2024-04-15 15:29:05 +0200

    Allow introspection into coroutine context from non-suspending environment
    This is required for https://youtrack.jetbrains.com/issue/IJPL-445
    
    # Conflicts:
    #	kotlinx-coroutines-core/js/src/CoroutineContext.kt
    #	kotlinx-coroutines-core/wasmJs/src/CoroutineContext.kt
    (moved the common change into jsAndWasmShared)

--- kotlinx-coroutines-core/jvm/src/internal/intellij/intellij.kt
+++ kotlinx-coroutines-core/jvm/src/internal/intellij/intellij.kt
@@ -1,6 +1,12 @@
+/**
+ * A special file that contains IntelliJ-related functions
+ */
 package kotlinx.coroutines.internal.intellij
 
 import kotlinx.coroutines.InternalCoroutinesApi
+import kotlin.coroutines.*
+
+internal val currentContextThreadLocal : ThreadLocal<CoroutineContext?> = ThreadLocal.withInitial { null }
 
 /**
  * [IntellijCoroutines] exposes the API added as part of IntelliJ patches.
@@ -9,4 +15,14 @@ import kotlinx.coroutines.InternalCoroutinesApi
 @InternalCoroutinesApi
 public object IntellijCoroutines {
 
+    /**
+     * IntelliJ Platform would like to introspect coroutine contexts outside the coroutine framework.
+     * This function is a non-suspend version of [coroutineContext].
+     *
+     * @return null if current thread is not used by coroutine dispatchers,
+     * or [coroutineContext] otherwise.
+     */
+    public fun currentThreadCoroutineContext(): CoroutineContext? {
+        return currentContextThreadLocal.get()
+    }
 }
