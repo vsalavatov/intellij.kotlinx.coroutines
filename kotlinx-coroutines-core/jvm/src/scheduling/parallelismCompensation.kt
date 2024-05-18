f7b409370e0e8672143c5fa130b8fe21e588669d
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2024-05-18 17:08:33 +0200

    Implement ParallelismCompensation for CoroutineScheduler

---
+++ kotlinx-coroutines-core/jvm/src/scheduling/parallelismCompensation.kt
@@ -0,0 +1,23 @@
+package kotlinx.coroutines.scheduling
+
+import kotlinx.coroutines.InternalCoroutinesApi
+
+/**
+ * Introduced as part of IntelliJ patches.
+ *
+ * Increases the parallelism limit of the coroutine dispatcher associated with the current thread for the duration of [body] execution.
+ * After the [body] completes, the effective parallelism may stay higher than the associated limit, but it is said
+ * that eventually it will adjust to meet it.
+ */
+@InternalCoroutinesApi
+public fun <T> compensateParallelism(body: () -> T): T {
+    // CoroutineScheduler.Worker implements ParallelismCompensation
+    val worker = Thread.currentThread() as? ParallelismCompensation
+        ?: return body()
+    worker.increaseParallelismAndLimit()
+    try {
+        return body()
+    } finally {
+        worker.decreaseParallelismLimit()
+    }
+}
