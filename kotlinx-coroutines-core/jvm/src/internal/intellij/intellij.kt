5212ddf502c31947e46e483c8aad836b9fddaf97
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2024-05-20 19:19:45 +0200

    add intellij.kt template

---
+++ kotlinx-coroutines-core/jvm/src/internal/intellij/intellij.kt
@@ -0,0 +1,12 @@
+package kotlinx.coroutines.internal.intellij
+
+import kotlinx.coroutines.InternalCoroutinesApi
+
+/**
+ * [IntellijCoroutines] exposes the API added as part of IntelliJ patches.
+ * Prefer to use the corresponding API from the IntelliJ Platform instead of accessing this object directly.
+ */
+@InternalCoroutinesApi
+public object IntellijCoroutines {
+
+}
