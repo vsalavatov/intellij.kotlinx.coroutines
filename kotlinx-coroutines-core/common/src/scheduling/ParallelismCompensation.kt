1cf661dad413a008595e9d37725961eb357c3328
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2024-05-20 20:44:15 +0200

    make ParallelismCompensation.kt internal

--- kotlinx-coroutines-core/common/src/scheduling/ParallelismCompensation.kt
+++ kotlinx-coroutines-core/common/src/scheduling/ParallelismCompensation.kt
@@ -1,23 +1,20 @@
 package kotlinx.coroutines.scheduling
 
-import kotlinx.coroutines.InternalCoroutinesApi
-
 /**
  * Introduced as part of IntelliJ patches.
  *
  * Runnables that are dispatched on [kotlinx.coroutines.CoroutineDispatcher] may optionally implement this interface
  * to declare an ability to compensate the associated parallelism resource.
  */
-@InternalCoroutinesApi
-public interface ParallelismCompensation {
+internal interface ParallelismCompensation {
     /**
      * Should increase both the limit and the effective parallelism.
      */
-    public fun increaseParallelismAndLimit()
+    fun increaseParallelismAndLimit()
 
     /**
      * Should only decrease the parallelism limit. The effective parallelism may temporarily stay higher than this limit.
      * Runnable should take care of checking whether effective parallelism needs to decrease to meet the desired limit.
      */
-    public fun decreaseParallelismLimit()
+    fun decreaseParallelismLimit()
 }
