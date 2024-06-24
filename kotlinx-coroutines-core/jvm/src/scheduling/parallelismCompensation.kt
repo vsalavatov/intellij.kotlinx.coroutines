680baa39228ff3dc3f1e051659c1992a591adb8f
Vadim Salavatov <vadim.salavatov@jetbrains.com>
2024-06-24 16:28:55 +0200

    parallelismCompensation: split and inline withCompensatedParallelism so that it is only present in the stacktraces when compensation is effective

--- kotlinx-coroutines-core/jvm/src/scheduling/parallelismCompensation.kt
+++ kotlinx-coroutines-core/jvm/src/scheduling/parallelismCompensation.kt
@@ -10,17 +10,22 @@ private val parallelismCompensationEnabled: Boolean =
  * After the [body] completes, the effective parallelism may stay higher than the associated limit, but it is said
  * that eventually it will adjust to meet it.
  */
-internal fun <T> withCompensatedParallelism(body: () -> T): T {
+@Suppress("NOTHING_TO_INLINE") // better stacktrace
+internal inline fun <T> withCompensatedParallelism(noinline body: () -> T): T {
     if (!parallelismCompensationEnabled) {
         return body()
     }
     // CoroutineScheduler.Worker implements ParallelismCompensation
     val worker = Thread.currentThread() as? ParallelismCompensation
         ?: return body()
-    worker.increaseParallelismAndLimit()
+    return worker.withCompensatedParallelism(body)
+}
+
+private fun <T> ParallelismCompensation.withCompensatedParallelism(body: () -> T): T {
+    increaseParallelismAndLimit()
     try {
         return body()
     } finally {
-        worker.decreaseParallelismLimit()
+        decreaseParallelismLimit()
     }
 }
